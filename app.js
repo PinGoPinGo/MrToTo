ef5a6c93ea7b70edaa1f98843fb622a5258ad463
karimation <selmi.kerim@gmail.com>
2017-04-10 13:43:20 +0100

    1

--- app.js
+++ app.js
@@ -268,7 +268,7 @@ function receivedMessage(event) {
 
           if (!error && response.statusCode === 200) {
               if(body['today']!=null){
-                sendTextMessage(senderID,body);
+                sendTextMessage(senderID,"body");
               }else{
                  sendTextMessage(senderID,"sorry !");   
               }
