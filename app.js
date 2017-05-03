1d783ee870e1a3b49b9c04d6e895eaf54531197b
karimation <selmi.kerim@gmail.com>
2017-05-03 04:15:36 +0100

    1

--- app.js
+++ app.js
@@ -102,7 +102,7 @@ app.post('/webhook', function (req, res) {
         } else if (messagingEvent.delivery) {
           sendMsg.receivedDeliveryConfirmation(messagingEvent);
         } else if (messagingEvent.postback) {
-          sendMsg.receivedPostback(messagingEvent);
+          receivedPostback(messagingEvent);
         } else if (messagingEvent.read) {
           sendMsg.receivedMessageRead(messagingEvent);
         } else if (messagingEvent.account_linking) {
@@ -287,6 +287,24 @@ function receivedMessage(event) {
   }
 }
 
+
+
+ function receivedPostback(event) {
+  var senderID = event.sender.id;
+  var recipientID = event.recipient.id;
+  var timeOfPostback = event.timestamp;
+
+  // The 'payload' param is a developer-defined field which is set in a postback 
+  // button for Structured Messages. 
+  var payload = event.postback.payload;
+
+  console.log("Received postback for user %d and page %d with payload '%s' " + 
+    "at %d", senderID, recipientID, payload, timeOfPostback);
+
+  // When a postback is called, we'll send a message back to the sender to 
+  // let them know it was successful
+  //sendTextMessage(senderID, "Postback called");
+}
 
  // Start server
 // Webhooks must be available via SSL with a certificate signed by a valid 
